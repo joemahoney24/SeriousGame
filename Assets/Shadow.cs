6db5967a107e6dbcb54607be404af866f910a7ce
Benjamin Tietjen <bentietjen@8200D-0777.DisStu.local>
2023-06-07 13:57:17 +0200

    added sandcastle obstacle

--- Assets/Shadow.cs
+++ Assets/Shadow.cs
@@ -4,17 +4,14 @@ using UnityEngine;
 
 public class Shadow : MonoBehaviour
 {
-    SpriteRenderer spriteRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        spriteRenderer.color = new Color(1f, 1f, 1f, 0.5f);
     }
 }
