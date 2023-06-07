4c294140d37c8145f9ba34f0dabd92d0a66310da
Benjamin Tietjen <bentietjen@Benjamins-MacBook-Pro.local>
2023-06-07 17:03:31 +0200

    Waterguy be moving

--- Assets/Shadow.cs
+++ Assets/Shadow.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Shadow : MonoBehaviour
-{
+{ 
+
 
     // Start is called before the first frame update
     void Start()
---
+++ Assets/WaterGuyScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterGuyScript : MonoBehaviour
+{
+
+    public float speed;
+    private int direction = 1;
+    protected Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 vel = rb2d.velocity;
+        vel.x = direction * speed;
+        rb2d.velocity = vel;
+    }
+}
